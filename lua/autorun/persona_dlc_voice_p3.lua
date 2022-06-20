626ce80460aef700b6a031c4c488b3ce0cc05475
Cpt-Hazama <Caleb123459@gmail.com>
2022-06-20 02:10:01 -0400

    a

---
+++ lua/autorun/persona_dlc_voice_p3.lua
@@ -0,0 +1,59 @@
+/*--------------------------------------------------
+	*** Copyright (c) 2012-2021 by Cpt. Hazama, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+--------------------------------------------------*/
+if (!file.Exists("autorun/persona_main.lua","LUA")) then return end
+
+P_AddVoice("aigis","start",{
+	"cpthazama/persona_dance/vo/aigis/08-037.wav",
+	"cpthazama/persona_dance/vo/aigis/08-038.wav",
+	"cpthazama/persona_dance/vo/aigis/08-039.wav",
+	"cpthazama/persona_dance/vo/aigis/08-040.wav",
+	"cpthazama/persona_dance/vo/aigis/08-041.wav",
+	"cpthazama/persona_dance/vo/aigis/08-042.wav",
+	"cpthazama/persona_dance/vo/aigis/08-049.wav",
+	"cpthazama/persona_dance/vo/aigis/08-052.wav",
+	"cpthazama/persona_dance/vo/aigis/08-054.wav",
+	"cpthazama/persona_dance/vo/aigis/08-058.wav",
+	"cpthazama/persona_dance/vo/aigis/08-061.wav",
+	"cpthazama/persona_dance/vo/aigis/08-064.wav",
+})
+
+P_AddVoice("aigis","winning",{
+	"cpthazama/persona_dance/vo/aigis/08-076.wav",
+	"cpthazama/persona_dance/vo/aigis/08-078.wav",
+	"cpthazama/persona_dance/vo/aigis/08-095.wav",
+	"cpthazama/persona_dance/vo/aigis/08-096.wav",
+	"cpthazama/persona_dance/vo/aigis/08-097.wav",
+	"cpthazama/persona_dance/vo/aigis/08-098.wav",
+	"cpthazama/persona_dance/vo/aigis/08-103.wav",
+	"cpthazama/persona_dance/vo/aigis/08-104.wav",
+	"cpthazama/persona_dance/vo/aigis/08-105.wav",
+	"cpthazama/persona_dance/vo/aigis/08-106.wav",
+	"cpthazama/persona_dance/vo/aigis/08-107.wav",
+})
+
+P_AddVoice("aigis","failing",{
+	"cpthazama/persona_dance/vo/aigis/08-099.wav",
+	"cpthazama/persona_dance/vo/aigis/08-100.wav",
+	"cpthazama/persona_dance/vo/aigis/08-101.wav",
+	"cpthazama/persona_dance/vo/aigis/08-102.wav",
+	"cpthazama/persona_dance/vo/aigis/08-127.wav",
+	"cpthazama/persona_dance/vo/aigis/08-128.wav",
+	"cpthazama/persona_dance/vo/aigis/08-129.wav",
+	"cpthazama/persona_dance/vo/aigis/08-130.wav",
+	"cpthazama/persona_dance/vo/aigis/08-131.wav",
+	"cpthazama/persona_dance/vo/aigis/08-132.wav",
+	"cpthazama/persona_dance/vo/aigis/08-134.wav",
+	"cpthazama/persona_dance/vo/aigis/08-136.wav",
+	"cpthazama/persona_dance/vo/aigis/08-137.wav",
+	"cpthazama/persona_dance/vo/aigis/08-138.wav",
+	"cpthazama/persona_dance/vo/aigis/08-150.wav",
+	"cpthazama/persona_dance/vo/aigis/08-151.wav",
+	"cpthazama/persona_dance/vo/aigis/08-152.wav",
+	"cpthazama/persona_dance/vo/aigis/08-153.wav",
+	"cpthazama/persona_dance/vo/aigis/08-154.wav",
+	"cpthazama/persona_dance/vo/aigis/08-155.wav",
+	"cpthazama/persona_dance/vo/aigis/08-156.wav",
+})
